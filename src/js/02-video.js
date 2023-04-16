460ef50ecba473b42c1c09c10ad4d15d60bf2d80
Filip Bączkowski <filo.b@wp.pl>
2023-04-16 23:19:53 +0200

    feat: task 2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+function saveTime(data) {
+  const { seconds } = data;
+  console.log(`${seconds}s`);
+  localStorage.setItem('videoplayer-current-time', seconds);
+  return seconds;
+}
+
+function setTime() {
+  const currentTime = JSON.parse(
+    localStorage.getItem('videoplayer-current-time')
+  );
+  player.setCurrentTime(currentTime);
+}
+
+player.on('timeupdate', throttle(saveTime, 1000, { leading: false }));
+
+window.addEventListener('load', setTime, { once: true });
