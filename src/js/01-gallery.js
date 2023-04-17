2130a0d976259cd4d8744b9dee741aef02fc1fcb
Filip Bączkowski <filo.b@wp.pl>
2023-04-18 01:32:00 +0200

    feat: first task completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,72 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+
+function galleryRender() {
+  let galleryArrayItems = [];
+
+  galleryItems.forEach((e, idx) => {
+    const { description, original, preview } = galleryItems[idx];
+
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+
+    a.classList.add('gallery__item');
+    img.classList.add('gallery__image');
+
+    a.setAttribute('href', original);
+    img.setAttribute('src', preview);
+    img.setAttribute('alt', description);
+
+    li.append(a);
+    a.append(img);
+
+    galleryArrayItems.push(li);
+  });
+
+  galleryList.append(...galleryArrayItems);
+}
+
+galleryRender();
+
+var lightbox = new SimpleLightbox('.gallery li a', {
+  captionsData: 'alt',
+  captionPosition: 'outside',
+  captionDelay: 250,
+  disableScroll: true,
+});
+
+function desktopScrollLockOff() {
+  let desktopBreakpoint = innerWidth > 1015 && window.scrollY === 0;
+
+  if (desktopBreakpoint) {
+    lightbox.options.disableScroll = false;
+    document.body.style.paddingRight = 0;
+  }
+  lightbox.options.disableScroll = true;
+}
+
+function openLightbox(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  desktopScrollLockOff();
+
+  lightbox.open(event.target);
+}
+
+galleryList.addEventListener('click', desktopScrollLockOff);
+galleryList.addEventListener('click', openLightbox);
+
+console.log(galleryItems);
+console.log(galleryList);
 
 console.log(galleryItems);
